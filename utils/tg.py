6330eeff58beb9a7ed4602ff3932fb8f9c34d236
nevelyov <87031773+nevelyov@users.noreply.github.com>
2026-01-15 22:20:50 +0300

    Update tg.py

--- utils/tg.py
+++ utils/tg.py
@@ -1,1 +1,10 @@
+import os
+import requests
 
+def send_telegram_message(text: str) -> None:
+    token = os.environ["TG_BOT_TOKEN"]
+    chat_id = os.environ["TG_CHAT_ID"]
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    payload = {"chat_id": chat_id, "text": text, "disable_web_page_preview": True}
+    r = requests.post(url, json=payload, timeout=30)
+    r.raise_for_status()
